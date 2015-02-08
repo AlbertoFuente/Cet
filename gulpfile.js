74af56cd715885a41fd4a091025bed039bb796be
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-02-08 09:14:36 +0100

    Config gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,30 @@
-/**
- * Created by Alberto on 8/2/15.
- */
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    watch = require('gulp-watch');
+
+gulp.task('default', function () {
+
+    gulp.src([
+        'app/components/table/createTable.js',
+        'app/components/table/tableEffects.js',
+        'app/components/table/tableEvents'
+    ])
+        .pipe(concat('cet.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js/'))
+});
+
+gulp.task('watch', function () {
+
+    gulp.watch([
+        'app/components/table/createTable.js',
+        'app/components/table/tableEffects.js',
+        'app/components/table/tableEvents'
+    ], function() {
+        gulp.start('default');
+    });
+
+});
+
