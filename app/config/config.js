8a6454b6dec636675e61d35b209cfd50a8361c18
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-02-08 09:30:21 +0100

    Config Script

--- app/config/config.js
+++ app/config/config.js
@@ -1,3 +1,27 @@
 /**
- * Created by Alberto on 8/2/15.
+ * _cetTable
+ * @type {Object} _cetTable - config Object
+ * @private
  */
+
+var _cetTable = {};
+// config type of data service
+_cetTable.dataOptions = {
+    'localData': true, // mode 1
+    'fireBase': false, // mode 2
+    'apiRest': false   // mode 3
+};
+// localData url
+_cetTable.localDataUrl = 'localData/tableData.json';
+// fireBase url
+_cetTable.fireBaseUrl = '';
+// apiRest url
+_cetTable.apiRestUrl = '';
+// display table effects
+_cetTable.effects = true;
+// sort table
+_cetTable.sortable = true;
+// div container
+_cetTable.container = document.getElementById('tableContainer');
+
+
