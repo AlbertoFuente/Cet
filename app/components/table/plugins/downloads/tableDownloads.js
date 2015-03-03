314653679ee6c05306655cb2bd41e6a48d4f2764
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-03-03 20:31:53 +0100

    tableDownloads.js changes

--- app/components/table/plugins/downloads/tableDownloads.js
+++ app/components/table/plugins/downloads/tableDownloads.js
@@ -2,40 +2,40 @@
  * table downloads
  */
 
-function tableDownloads () {
+var tableDownloads = () => {
     // import table Export
-    var importTbExport = document.createElement('script');
+    let importTbExport = document.createElement('script');
     importTbExport.src = '../../bower_components/table-export/tableExport.js';
     document.head.appendChild(importTbExport);
     // import base64
-    var b64 = document.createElement('script');
+    let b64 = document.createElement('script');
     b64.src = '../../bower_components/table-export/jquery.base64.js';
     document.head.appendChild(b64);
     // html 2 canvas
-    var html2Canvas = document.createElement('script');
+    let html2Canvas = document.createElement('script');
     html2Canvas.src = '../../bower_components/table-export/html2canvas.js';
     document.head.appendChild(html2Canvas);
     // jspdf
-    var jspdf = document.createElement('script');
+    let jspdf = document.createElement('script');
     jspdf.src = '../../bower_components/table-export/jspdf/jspdf.js';
     document.head.appendChild(jspdf);
     // jspdf base64
-    var jspdfB64 = document.createElement('script');
+    let jspdfB64 = document.createElement('script');
     jspdfB64.src = '../../bower_components/table-export/jspdf/libs/base64.js';
     document.head.appendChild(jspdfB64);
     // jspdf sprintf
-    var sprintF = document.createElement('script');
+    let sprintF = document.createElement('script');
     sprintF.src = '../../bower_components/table-export/jspdf/libs/sprintf.js';
     document.head.appendChild(sprintF);
-}
+};
 
 /**
  * selected option to download
  * @param selected
  */
 
-function selectedOption (selected) {
-    var tableId = $('#cetTable');
+var selectedOption = (selected) => {
+    let tableId = $('#cetTable');
     switch (selected) {
         case 'json':
             tableId.tableExport({type:selected,escape:'false'});
@@ -69,4 +69,4 @@ function selectedOption (selected) {
             break;
     }
 
-}
+};
