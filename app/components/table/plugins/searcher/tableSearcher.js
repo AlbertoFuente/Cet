66f726cc9a809d99a6e3d973137c81a2052cb385
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-03-09 22:26:53 +0100

    Changed the structure of the function, now is part of the global object CET inside CET.search

--- app/components/table/plugins/searcher/tableSearcher.js
+++ app/components/table/plugins/searcher/tableSearcher.js
@@ -1,70 +1,72 @@
-/**
- * table searcher
- * @param val
- * @param tableData
- */
+CET.search = {
+    /**
+     * table searcher
+     * @param val
+     * @param tableData
+     */
 
-var tableSearcher = (val, tableData) => {
-    let obj = {};
-    obj.defaultData = tableData[0].body;
-    obj.newData = [];
+    tableSearcher: (val, tableData) => {
+        let obj = {};
+        obj.defaultData = tableData[0].body;
+        obj.newData = [];
 
-    var refreshTable = (obj) => {
-        let table = document.getElementById('cetTable'),
-            tableBody = table.childNodes[1];
+        var refreshTable = (obj) => {
+            let table = document.getElementById('cetTable'),
+                tableBody = table.childNodes[1];
 
-        for (let i = 0; i < obj.length; i++) {
-            for (let j in tableBody.childNodes) {
-                if (obj[i].tr !== tableBody.childNodes[j].className) {
-                    if (typeof tableBody.childNodes[j] === 'object') {
-                        if (!tableBody.childNodes[j].hasAttribute('style')) {
-                            tableBody.childNodes[j].style.opacity = '0.3'; //For real browsers;
-                            tableBody.childNodes[j].style.filter = "alpha(opacity=30)"; //For IE;
+            for (let i = 0; i < obj.length; i++) {
+                for (let j in tableBody.childNodes) {
+                    if (obj[i].tr !== tableBody.childNodes[j].className) {
+                        if (typeof tableBody.childNodes[j] === 'object') {
+                            if (!tableBody.childNodes[j].hasAttribute('style')) {
+                                tableBody.childNodes[j].style.opacity = '0.3'; //For real browsers;
+                                tableBody.childNodes[j].style.filter = "alpha(opacity=30)"; //For IE;
+                            }
                         }
+                    } else {
+                        tableBody.childNodes[j].style.opacity = '1'; //For real browsers;
+                        tableBody.childNodes[j].style.filter = "alpha(opacity=100)"; //For IE;
                     }
-                } else {
-                    tableBody.childNodes[j].style.opacity = '1'; //For real browsers;
-                    tableBody.childNodes[j].style.filter = "alpha(opacity=100)"; //For IE;
                 }
             }
-        }
-    };
+        };
 
-    var cleanTable = () => {
-        let table = document.getElementById('cetTable'),
-            tableBody = table.childNodes[1];
-        for (let i in tableBody.childNodes) {
-            if (typeof tableBody.childNodes[i] === 'object') {
-                tableBody.childNodes[i].removeAttribute('style');
-            }
-        }
-    };
-
-    let lowerVal = val.toLowerCase();
-
-    if (val != '') {
-        for (let i in obj.defaultData) {
-
-            for (let f in obj.defaultData[i]) {
-                var data = obj.defaultData[i][f].data.toLowerCase();
-                if (data.search(lowerVal) != -1) {
-                    obj.newData.push({
-                        'tr': i,
-                        'tds': obj.defaultData[i]
-                    });
+        var cleanTable = () => {
+            let table = document.getElementById('cetTable'),
+                tableBody = table.childNodes[1];
+            for (let i in tableBody.childNodes) {
+                if (typeof tableBody.childNodes[i] === 'object') {
+                    tableBody.childNodes[i].removeAttribute('style');
                 }
             }
-        }
-        try {
-            cleanTable();
-        } finally {
-            refreshTable(obj.newData);
-        }
-    } else {
-        try {
-            cleanTable();
-        } finally {
-            refreshTable(obj.defaultData);
+        };
+
+        let lowerVal = val.toLowerCase();
+
+        if (val != '') {
+            for (let i in obj.defaultData) {
+
+                for (let f in obj.defaultData[i]) {
+                    var data = obj.defaultData[i][f].data.toLowerCase();
+                    if (data.search(lowerVal) != -1) {
+                        obj.newData.push({
+                            'tr': i,
+                            'tds': obj.defaultData[i]
+                        });
+                    }
+                }
+            }
+            try {
+                cleanTable();
+            } finally {
+                refreshTable(obj.newData);
+            }
+        } else {
+            try {
+                cleanTable();
+            } finally {
+                refreshTable(obj.defaultData);
+            }
         }
     }
-};
+}
