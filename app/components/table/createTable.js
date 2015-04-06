5953c2854922c4a6044bd4b66fa340e699d7a77c
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-04-06 21:35:43 +0200

    Fixed bug in createTable.js

--- app/components/table/createTable.js
+++ app/components/table/createTable.js
@@ -165,7 +165,7 @@
                     window.onresize = () => {
                         let elementPosition = tableHeaderOptions.getBoundingClientRect();
 
-                        if (optionsContainer.style.display = 'block') {
+                        if (optionsContainer.style.display === 'block') {
                             optionsContainer.style.top = (elementPosition.top + 38) + 'px';
                             optionsContainer.style.left = (elementPosition.left - 164) + 'px';
                         }
