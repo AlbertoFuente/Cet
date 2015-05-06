5337320e56fe7c56ce57d51058896b45ac723bd2
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-05-06 21:19:27 +0200

    Improve plugins code

--- app/components/table/plugins/searcher/tableSearcher.js
+++ app/components/table/plugins/searcher/tableSearcher.js
@@ -1,77 +1,78 @@
 ((cet) => {
-    // CET.search object
-    cet.search = {};
-    
-    /**
-     * table searcher
-     * @param val
-     * @param tableData
-     */
+	'use strict';
+	// CET.search object
+	cet.search = {};
 
-    cet.search.tableSearcher = (val, tableData) => {
-        let obj = {};
-        obj.defaultData = tableData[0].body;
-        obj.newData = [];
+	/**
+	 * table searcher
+	 * @param val
+	 * @param tableData
+	 */
 
-        var refreshTable = (obj) => {
-            let table = document.getElementById('cetTable'),
-                tableBody = table.childNodes[1];
+	cet.search.tableSearcher = (val, tableData) => {
+		let obj = {};
+		obj.defaultData = tableData[0].body;
+		obj.newData = [];
 
-            for (let i = 0; i < obj.length; i++) {
-                for (let j in tableBody.childNodes) {
-                    if (obj[i].tr !== tableBody.childNodes[j].className) {
-                        if (tableBody.childNodes[j].tagName === 'TR') {
-                            if (!tableBody.childNodes[j].hasAttribute('style')) {
-                                tableBody.childNodes[j].style.opacity = '0.3'; //For real browsers;
-                                tableBody.childNodes[j].style.filter = "alpha(opacity=30)"; //For IE;
-                            }
-                        }
-                    } else {
-                        tableBody.childNodes[j].style.opacity = '1'; //For real browsers;
-                        tableBody.childNodes[j].style.filter = "alpha(opacity=100)"; //For IE;
-                    }
-                }
-            }
-        };
+		var refreshTable = (obj) => {
+			let table = document.getElementById('cetTable'),
+				tableBody = table.childNodes[1];
 
-        var cleanTable = () => {
-            let table = document.getElementById('cetTable'),
-                tableBody = table.childNodes[1];
-            for (let i in tableBody.childNodes) {
-                if (tableBody.childNodes[i].tagName === 'TR') {
-                    if (tableBody.childNodes[i].hasAttribute('style')) {
-                        tableBody.childNodes[i].removeAttribute('style');
-                    }
-                }
-            }
-        };
+			for (let i = 0; i < obj.length; i++) {
+				for (let j in tableBody.childNodes) {
+					if (obj[i].tr !== tableBody.childNodes[j].className) {
+						if (tableBody.childNodes[j].tagName === 'TR') {
+							if (!tableBody.childNodes[j].hasAttribute('style')) {
+								tableBody.childNodes[j].style.opacity = '0.3'; //For real browsers;
+								tableBody.childNodes[j].style.filter = "alpha(opacity=30)"; //For IE;
+							}
+						}
+					} else {
+						tableBody.childNodes[j].style.opacity = '1'; //For real browsers;
+						tableBody.childNodes[j].style.filter = "alpha(opacity=100)"; //For IE;
+					}
+				}
+			}
+		};
 
-        let lowerVal = val.toLowerCase();
+		var cleanTable = () => {
+			let table = document.getElementById('cetTable'),
+				tableBody = table.childNodes[1];
+			for (let i in tableBody.childNodes) {
+				if (tableBody.childNodes[i].tagName === 'TR') {
+					if (tableBody.childNodes[i].hasAttribute('style')) {
+						tableBody.childNodes[i].removeAttribute('style');
+					}
+				}
+			}
+		};
 
-        if (val !== '') {
-            for (let i in obj.defaultData) {
+		let lowerVal = val.toLowerCase();
 
-                for (let f in obj.defaultData[i]) {
-                    var data = obj.defaultData[i][f].data.toLowerCase();
-                    if (data.search(lowerVal) !== -1) {
-                        obj.newData.push({
-                            'tr': i,
-                            'tds': obj.defaultData[i]
-                        });
-                    }
-                }
-            }
-            try {
-                cleanTable();
-            } finally {
-                refreshTable(obj.newData);
-            }
-        } else {
-            try {
-                cleanTable();
-            } finally {
-                refreshTable(obj.defaultData);
-            }
-        }
-    };
+		if (val !== '') {
+			for (let i in obj.defaultData) {
+
+				for (let f in obj.defaultData[i]) {
+					var data = obj.defaultData[i][f].data.toLowerCase();
+					if (data.search(lowerVal) !== -1) {
+						obj.newData.push({
+							'tr': i,
+							'tds': obj.defaultData[i]
+						});
+					}
+				}
+			}
+			try {
+				cleanTable();
+			} finally {
+				refreshTable(obj.newData);
+			}
+		} else {
+			try {
+				cleanTable();
+			} finally {
+				refreshTable(obj.defaultData);
+			}
+		}
+	};
 })(CET || {});
