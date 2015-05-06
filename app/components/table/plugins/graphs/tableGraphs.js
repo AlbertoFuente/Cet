6cc009c92059303b76a001b16da9ae6ef655fb99
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-05-06 22:33:05 +0200

    Improve table plugins

--- app/components/table/plugins/graphs/tableGraphs.js
+++ app/components/table/plugins/graphs/tableGraphs.js
@@ -1,259 +1,280 @@
 ((cet) => {
-	'use strict';
-	// CET.graphs object
-	cet.graphs = {};
+    'use strict';
+    // CET.graphs object
+    cet.graphs = {};
 
-	// Error Tokens
-	const tokenErrorCreateGraph = 'Impossible to construct a graph of the selected data, try another selection.';
+    // Error Tokens
+    const tokenErrorCreateGraph = 'Impossible to construct a graph of the selected data, try another selection.';
 
-	/**
-	 * Show modal
-	 */
+    /**
+     * Show modal
+     */
 
-	cet.graphs.showModal = () => {
-		let gModal = document.createElement('div');
-		gModal.className = 'gModal';
-		gModal.id = 'gModal';
-		let gHeader = document.createElement('div');
-		gHeader.className = 'gHeader';
-		let closeButton = document.createElement('button');
-		closeButton.className = 'closeButton ' + cet.table.assignClasses('closeButton');
+    cet.graphs.showModal = () => {
+        let CET = CET;
+        let gModal = document.createElement('div');
+        gModal.className = 'gModal';
+        gModal.id = 'gModal';
+        let gHeader = document.createElement('div');
+        gHeader.className = 'gHeader';
+        let closeButton = document.createElement('button');
+        closeButton.className = 'closeButton ' + cet.table.assignClasses('closeButton');
 
-		closeButton.onclick = () => CET.options.closeModal(gModal);
+        closeButton.onclick = () => CET.options.closeModal(gModal);
 
-		let gContent = document.createElement('canvas');
-		gContent.className = 'gContent';
-		gContent.id = 'chart';
-		gContent.width = '700';
-		gContent.height = '350';
+        let gContent = document.createElement('canvas');
+        gContent.className = 'gContent';
+        gContent.id = 'chart';
+        gContent.width = '700';
+        gContent.height = '350';
 
-		gHeader.appendChild(closeButton);
-		gModal.appendChild(gHeader);
-		gModal.appendChild(gContent);
+        gHeader.appendChild(closeButton);
+        gModal.appendChild(gHeader);
+        gModal.appendChild(gContent);
 
-		document.body.appendChild(gModal);
-	};
+        document.body.appendChild(gModal);
+    };
 
-	/**
-	 * Show modal graph
-	 * @param graphType
-	 * @param xData
-	 * @param yData
-	 */
+    /**
+     * Show modal graph
+     * @param graphType
+     * @param xData
+     * @param yData
+     */
 
-	cet.graphs.showGraph = (graphType, xData, yData) => {
+    cet.graphs.showGraph = (graphType, xData, yData) => {
 
-		var colors = (index) => {
-			let colors = [
-				'rgba(244, 67, 54, 0.5)',
-				'rgba(233, 30, 99, 0.5)',
-				'rgba(156, 39, 176, 0.5)',
-				'rgba(103, 58, 183, 0.5)',
-				'rgba(63, 81, 181, 0.5)',
-				'rgba(33, 150, 143, 0.5)',
-				'rgba(3, 169, 244, 0.5)',
-				'rgba(0, 188, 212, 0.5)',
-				'rgba(0, 150, 136, 0.5)',
-				'rgba(76, 175, 80, 0.5)',
-				'rgba(139, 195, 74, 0.5)',
-				'rgba(205, 220, 57, 0.5)',
-				'rgba(255, 235, 59, 0.5)',
-				'rgba(255, 193, 7, 0.5)',
-				'rgba(255, 152, 0, 0.5)',
-				'rgba(255, 87, 34, 0.5)',
-				'rgba(121, 85, 72, 0.5)'
-			];
+        var colors = (index) => {
+            let colors = [
+                'rgba(244, 67, 54, 0.5)',
+                'rgba(233, 30, 99, 0.5)',
+                'rgba(156, 39, 176, 0.5)',
+                'rgba(103, 58, 183, 0.5)',
+                'rgba(63, 81, 181, 0.5)',
+                'rgba(33, 150, 143, 0.5)',
+                'rgba(3, 169, 244, 0.5)',
+                'rgba(0, 188, 212, 0.5)',
+                'rgba(0, 150, 136, 0.5)',
+                'rgba(76, 175, 80, 0.5)',
+                'rgba(139, 195, 74, 0.5)',
+                'rgba(205, 220, 57, 0.5)',
+                'rgba(255, 235, 59, 0.5)',
+                'rgba(255, 193, 7, 0.5)',
+                'rgba(255, 152, 0, 0.5)',
+                'rgba(255, 87, 34, 0.5)',
+                'rgba(121, 85, 72, 0.5)'
+            ];
 
-			if (index > colors.length) return colors[Math.floor(Math.random() * colors.length)];
-			else return colors[index];
-		};
+            if (index > colors.length) return colors[Math.floor(Math.random() * colors.length)];
+            else return colors[index];
+        };
 
-		switch (graphType) {
-			case 'Bar chart':
-				// prepare data
-				let data = {
-					'labels': [],
-					'datasets': [{
-						'data': [],
-						'label': 'title A',
-						'fillColor': 'rgba(220,220,220,0.5)',
-						'strokeColor': 'rgba(220,220,220,0.8)',
-						'highlightFill': 'rgba(220,220,220,0.75)',
-						'highlightStroke': 'rgba(220,220,220,1)'
-					}]
-				};
-				for (let i in xData) {
-					data.labels.push(xData[i]);
-				}
-				for (let i in yData) {
-					data.datasets[0].data.push(parseInt(yData[i]));
-				}
+        switch (graphType) {
+            case 'Bar chart':
+                // prepare data
+                let data = {
+                    'labels': [],
+                    'datasets': [{
+                        'data': [],
+                        'label': 'title A',
+                        'fillColor': 'rgba(220,220,220,0.5)',
+                        'strokeColor': 'rgba(220,220,220,0.8)',
+                        'highlightFill': 'rgba(220,220,220,0.75)',
+                        'highlightStroke': 'rgba(220,220,220,1)'
+                    }]
+                };
+                for (let i in xData) {
+                    if (xData.hasOwnProperty(i)) {
+                        data.labels.push(xData[i]);
+                    }
+                }
+                for (let i in yData) {
+                    if (yData.hasOwnProperty(i)) {
+                        data.datasets[0].data.push(parseInt(yData[i]));
+                    }
+                }
 
-				// print graph
-				let bar = document.getElementById("chart").getContext("2d"),
-					BarChart = new Chart(bar).Bar(data);
+                // print graph
+                let bar = document.getElementById("chart").getContext("2d"),
+                    barChart = new Chart(bar).Bar(data);
 
-				for (let i in xData) {
-					BarChart.datasets[0].bars[i].fillColor = colors(i);
-					BarChart.datasets[0].bars[i].strokeColor = colors(i);
-				}
-				BarChart.update();
+                for (let i in xData) {
+                    if (xData.hasOwnProperty(i)) {
+                        barChart.datasets[0].bars[i].fillColor = colors(i);
+                        barChart.datasets[0].bars[i].strokeColor = colors(i);
+                    }
+                }
+                barChart.update();
 
-				break;
-			case 'Line chart':
-				// prepare data
-				let dataLine = {
-					'labels': [],
-					'datasets': [{
-						'data': [],
-						'label': 'title A',
-						'fillColor': 'rgba(244, 67, 54, 0.5)',
-						'strokeColor': 'rgba(244, 67, 54, 0.8)',
-						'highlightFill': 'rgba(220,220,220,0.75)',
-						'highlightStroke': 'rgba(220,220,220,1)'
-					}]
-				};
-				for (let i in xData) {
-					dataLine.labels.push(xData[i]);
-				}
-				for (let i in yData) {
-					dataLine.datasets[0].data.push(parseInt(yData[i]));
-				}
+                break;
+            case 'Line chart':
+                // prepare data
+                let dataLine = {
+                    'labels': [],
+                    'datasets': [{
+                        'data': [],
+                        'label': 'title A',
+                        'fillColor': 'rgba(244, 67, 54, 0.5)',
+                        'strokeColor': 'rgba(244, 67, 54, 0.8)',
+                        'highlightFill': 'rgba(220,220,220,0.75)',
+                        'highlightStroke': 'rgba(220,220,220,1)'
+                    }]
+                };
+                for (let i in xData) {
+                    if (xData.hasOwnProperty(i)) {
+                        dataLine.labels.push(xData[i]);
+                    }
+                }
+                for (let i in yData) {
+                    if (yData.hasOwnProperty(i)) {
+                        dataLine.datasets[0].data.push(parseInt(yData[i]));
+                    }
+                }
 
-				// print graph
-				let line = document.getElementById("chart").getContext("2d"),
-					LineChart = new Chart(line).Line(dataLine);
+                // print graph
+                let line = document.getElementById("chart").getContext("2d"),
+                    LineChart = new Chart(line).Line(dataLine);
 
-				break;
-			case 'Pie chart':
-				let dataPie = [];
-				for (let i in xData) {
-					dataPie.push({
-						'label': xData[i],
-						'value': parseInt(yData[i]),
-						'color': colors(i),
-						'highlight': colors(i)
-					});
-				}
-				// print graph
-				let pie = document.getElementById("chart").getContext("2d"),
-					myPieChart = new Chart(pie).Doughnut(dataPie);
-				break;
-			case 'Polar Area chart':
-				let dataPolar = [];
-				for (let i in xData) {
-					dataPolar.push({
-						'label': xData[i],
-						'value': parseInt(yData[i]),
-						'color': colors(i),
-						'highlight': colors(i)
-					});
-				}
-				// print graph
-				let polar = document.getElementById("chart").getContext("2d"),
-					myPolarChart = new Chart(polar).PolarArea(dataPolar);
-				break;
-		}
-	};
+                break;
+            case 'Pie chart':
+                let dataPie = [];
+                for (let i in xData) {
+                    if (xData.hasOwnProperty(i)) {
+                        dataPie.push({
+                            'label': xData[i],
+                            'value': parseInt(yData[i]),
+                            'color': colors(i),
+                            'highlight': colors(i)
+                        });
+                    }
+                }
+                // print graph
+                let pie = document.getElementById("chart").getContext("2d"),
+                    myPieChart = new Chart(pie).Doughnut(dataPie);
+                break;
+            case 'Polar Area chart':
+                let dataPolar = [];
+                for (let i in xData) {
+                    if (xData.hasOwnProperty(i)) {
+                        dataPolar.push({
+                            'label': xData[i],
+                            'value': parseInt(yData[i]),
+                            'color': colors(i),
+                            'highlight': colors(i)
+                        });
+                    }
+                }
+                // print graph
+                let polar = document.getElementById("chart").getContext("2d"),
+                    myPolarChart = new Chart(polar).PolarArea(dataPolar);
+                break;
+        }
+    };
 
-	/**
-	 * prepare the data for print the graph
-	 * @param cet
-	 * @param selecteds
-	 * @param modalContainer
-	 */
+    /**
+     * prepare the data for print the graph
+     * @param cet
+     * @param selecteds
+     * @param modalContainer
+     */
 
-	cet.graphs.prepareGraphData = (cet, selecteds, modalContainer) => {
-		let graphType = selecteds.type,
-			xAxis = selecteds.xAxis,
-			yAxis = selecteds.yAxis,
-			tData = cet.tableData ||  cet.defaultConfig.tableData,
-			tableHead = tData[0].head,
-			tableBody = tData[0].body,
-			xNum = null,
-			yNum = null,
-			xData = [],
-			yData = [],
-			compareArrays = {};
+    cet.graphs.prepareGraphData = (cet, selecteds, modalContainer) => {
+        let graphType = selecteds.type,
+            xAxis = selecteds.xAxis,
+            yAxis = selecteds.yAxis,
+            tData = cet.tableData || cet.defaultConfig.tableData,
+            tableHead = tData[0].head,
+            tableBody = tData[0].body,
+            xNum = null,
+            yNum = null,
+            xData = [],
+            yData = [],
+            compareArrays = {};
 
-		// tableHead
-		for (let key in tableHead) {
-			if (tableHead[key] === xAxis) xNum = key.slice(-1);
+        // tableHead
+        for (let key in tableHead) {
+            if (tableHead.hasOwnProperty(key)) {
+                if (tableHead[key] === xAxis) xNum = key.slice(-1);
 
-			if (tableHead[key] === yAxis) yNum = key.slice(-1);
-		}
+                if (tableHead[key] === yAxis) yNum = key.slice(-1);
+            }
+        }
 
-		// tableBody
-		for (let key in tableBody) {
-			for (let k in tableBody[key]) {
-				let tdNumX = 'td' + xNum,
-					tdNumY = 'td' + yNum;
-				// save xAxis data
-				if (tdNumX === k) xData.push(tableBody[key][k].data);
-				// save yAxis data
-				if (tdNumY === k) yData.push(tableBody[key][k].data);
-			}
-		}
+        // tableBody
+        for (let key in tableBody) {
+            if (tableBody.hasOwnProperty(key)) {
+                for (let k in tableBody[key]) {
+                    if (tableBody[key].hasOwnProperty(k)) {
+                        let tdNumX = 'td' + xNum,
+                            tdNumY = 'td' + yNum;
+                        // save xAxis data
+                        if (tdNumX === k) xData.push(tableBody[key][k].data);
+                        // save yAxis data
+                        if (tdNumY === k) yData.push(tableBody[key][k].data);
+                    }
+                }
+            }
+        }
 
-		// compare arrays
-		// array length
-		if (xData.length > 0 && xData.length === yData.length) compareArrays.sameLength = true;
+        // compare arrays
+        // array length
+        if (xData.length > 0 && xData.length === yData.length) compareArrays.sameLength = true;
 
-		// extend String Class with isNumeric fucntion
-		if (!String.prototype.isNumeric) {
-			String.prototype.isNumeric = function() {
-				return !isNaN(parseFloat(this));
-			};
-		}
+        // extend String Class with isNumeric fucntion
+        if (!String.prototype.isNumeric) {
+            String.prototype.isNumeric = function() {
+                return !isNaN(parseFloat(this));
+            };
+        }
 
-		// xData
-		for (let i = 0; i < xData.length; i++) {
-			if (xData[i].isNumeric()) {
-				compareArrays.xAxisIsNum = true;
-			} else {
-				compareArrays.xAxisIsNum = false;
-				break;
-			}
-		}
-		// yData
-		for (let j = 0; j < yData.length; j++) {
-			if (yData[j].isNumeric()) {
-				compareArrays.yAxisIsNum = true;
-			} else {
-				compareArrays.yAxisIsNum = false;
-				break;
-			}
-		}
+        // xData
+        for (let i = 0; i < xData.length; i++) {
+            if (xData[i].isNumeric()) {
+                compareArrays.xAxisIsNum = true;
+            } else {
+                compareArrays.xAxisIsNum = false;
+                break;
+            }
+        }
+        // yData
+        for (let j = 0; j < yData.length; j++) {
+            if (yData[j].isNumeric()) {
+                compareArrays.yAxisIsNum = true;
+            } else {
+                compareArrays.yAxisIsNum = false;
+                break;
+            }
+        }
 
-		/**
-		 * error data
-		 * @param cont
-		 */
+        /**
+         * error data
+         * @param cont
+         */
 
-		var showError = (cont) => {
-			let errLabel = document.createElement('label');
-			errLabel.className = 'errLabel';
-			errLabel.innerHTML = tokenErrorCreateGraph;
-			cont.appendChild(errLabel);
-			setTimeout(() => {
-				cont.removeChild(errLabel);
-			}, 5000);
-		};
+        var showError = (cont) => {
+            let errLabel = document.createElement('label');
+            errLabel.className = 'errLabel';
+            errLabel.innerHTML = tokenErrorCreateGraph;
+            cont.appendChild(errLabel);
+            setTimeout(() => {
+                cont.removeChild(errLabel);
+            }, 5000);
+        };
 
-		// check if everything is ok
-		if (compareArrays.sameLength) {
-			if (compareArrays.xAxisIsNum !== compareArrays.yAxisIsNum) {
-				modalContainer.parentNode.style.display = 'none';
-				cet.graphs.showModal();
+        // check if everything is ok
+        if (compareArrays.sameLength) {
+            if (compareArrays.xAxisIsNum !== compareArrays.yAxisIsNum) {
+                modalContainer.parentNode.style.display = 'none';
+                cet.graphs.showModal();
 
-				if (compareArrays.xAxisIsNum) {
-					cet.graphs.showGraph(graphType, yData, xData);
-				} else {
-					cet.graphs.showGraph(graphType, xData, yData);
-				}
-			} else {
-				showError(modalContainer);
-			}
-		}
-	};
+                if (compareArrays.xAxisIsNum) {
+                    cet.graphs.showGraph(graphType, yData, xData);
+                } else {
+                    cet.graphs.showGraph(graphType, xData, yData);
+                }
+            } else {
+                showError(modalContainer);
+            }
+        }
+    };
 })(CET || {});
--- app/components/table/plugins/searcher/tableSearcher.js
+++ app/components/table/plugins/searcher/tableSearcher.js
@@ -1,78 +1,81 @@
 ((cet) => {
-	'use strict';
-	// CET.search object
-	cet.search = {};
+    'use strict';
+    // CET.search object
+    cet.search = {};
 
-	/**
-	 * table searcher
-	 * @param val
-	 * @param tableData
-	 */
+    /**
+     * table searcher
+     * @param val
+     * @param tableData
+     */
 
-	cet.search.tableSearcher = (val, tableData) => {
-		let obj = {};
-		obj.defaultData = tableData[0].body;
-		obj.newData = [];
+    cet.search.tableSearcher = (val, tableData) => {
+        let obj = {};
+        obj.defaultData = tableData[0].body;
+        obj.newData = [];
 
-		var refreshTable = (obj) => {
-			let table = document.getElementById('cetTable'),
-				tableBody = table.childNodes[1];
+        var refreshTable = (obj) => {
+            let table = document.getElementById('cetTable'),
+                tableBody = table.childNodes[1];
 
-			for (let i = 0; i < obj.length; i++) {
-				for (let j in tableBody.childNodes) {
-					if (obj[i].tr !== tableBody.childNodes[j].className) {
-						if (tableBody.childNodes[j].tagName === 'TR') {
-							if (!tableBody.childNodes[j].hasAttribute('style')) {
-								tableBody.childNodes[j].style.opacity = '0.3'; //For real browsers;
-								tableBody.childNodes[j].style.filter = "alpha(opacity=30)"; //For IE;
-							}
-						}
-					} else {
-						tableBody.childNodes[j].style.opacity = '1'; //For real browsers;
-						tableBody.childNodes[j].style.filter = "alpha(opacity=100)"; //For IE;
-					}
-				}
-			}
-		};
+            for (let i = 0; i < obj.length; i++) {
+                for (let j in tableBody.childNodes) {
+                    if (obj[i].tr !== tableBody.childNodes[j].className) {
+                        if (tableBody.childNodes[j].tagName === 'TR') {
+                            if (!tableBody.childNodes[j].hasAttribute('style')) {
+                                tableBody.childNodes[j].style.opacity = '0.3'; //For real browsers;
+                                tableBody.childNodes[j].style.filter = "alpha(opacity=30)"; //For IE;
+                            }
+                        }
+                    } else {
+                        tableBody.childNodes[j].style.opacity = '1'; //For real browsers;
+                        tableBody.childNodes[j].style.filter = "alpha(opacity=100)"; //For IE;
+                    }
+                }
+            }
+        };
 
-		var cleanTable = () => {
-			let table = document.getElementById('cetTable'),
-				tableBody = table.childNodes[1];
-			for (let i in tableBody.childNodes) {
-				if (tableBody.childNodes[i].tagName === 'TR') {
-					if (tableBody.childNodes[i].hasAttribute('style')) {
-						tableBody.childNodes[i].removeAttribute('style');
-					}
-				}
-			}
-		};
+        var cleanTable = () => {
+            let table = document.getElementById('cetTable'),
+                tableBody = table.childNodes[1];
+            for (let i in tableBody.childNodes) {
+                if (tableBody.childNodes[i].tagName === 'TR') {
+                    if (tableBody.childNodes[i].hasAttribute('style')) {
+                        tableBody.childNodes[i].removeAttribute('style');
+                    }
+                }
+            }
+        };
 
-		let lowerVal = val.toLowerCase();
+        let lowerVal = val.toLowerCase();
 
-		if (val !== '') {
-			for (let i in obj.defaultData) {
-
-				for (let f in obj.defaultData[i]) {
-					var data = obj.defaultData[i][f].data.toLowerCase();
-					if (data.search(lowerVal) !== -1) {
-						obj.newData.push({
-							'tr': i,
-							'tds': obj.defaultData[i]
-						});
-					}
-				}
-			}
-			try {
-				cleanTable();
-			} finally {
-				refreshTable(obj.newData);
-			}
-		} else {
-			try {
-				cleanTable();
-			} finally {
-				refreshTable(obj.defaultData);
-			}
-		}
-	};
+        if (val !== '') {
+            for (let i in obj.defaultData) {
+                if (obj.defaultData.hasOwnProperty(i)) {
+                    for (let f in obj.defaultData[i]) {
+                        if (obj.defaultData[i].hasOwnProperty(f)) {
+                            var data = obj.defaultData[i][f].data.toLowerCase();
+                            if (data.search(lowerVal) !== -1) {
+                                obj.newData.push({
+                                    'tr': i,
+                                    'tds': obj.defaultData[i]
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            try {
+                cleanTable();
+            } finally {
+                refreshTable(obj.newData);
+            }
+        } else {
+            try {
+                cleanTable();
+            } finally {
+                refreshTable(obj.defaultData);
+            }
+        }
+    };
 })(CET || {});
